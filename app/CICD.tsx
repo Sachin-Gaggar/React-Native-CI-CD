2f736c03e5c99f35ecac9c417936d26a87b4c61c
Sachin Gaggar <sachin.gaggar@tothenew.com>
2024-09-27 13:53:45 +0530

    intial commit

---
+++ app/CICD.tsx
@@ -0,0 +1,16 @@
+import { SplashScreen } from "expo-router";
+import React, { useEffect } from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+type Props = {};
+const CICD = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Sachin</Text>
+    </View>
+  );
+};
+export default CICD;
+const styles = StyleSheet.create({
+  container: {},
+});
